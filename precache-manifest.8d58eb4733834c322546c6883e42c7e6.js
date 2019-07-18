5272a84dc1928a8716faad150cf128de7a2a2dad
iqyoe <muhiqballukman@gmail.com>
2019-07-18 22:07:20 +0700

    Updates

---
+++ precache-manifest.8d58eb4733834c322546c6883e42c7e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "971cc87e20a838a74f533e1f110d7310",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "1b4b62a1b06b78af50fa",
+    "url": "/resume/static/css/main.8ee4eb84.chunk.css"
+  },
+  {
+    "revision": "78626fa88d78b1203a04",
+    "url": "/resume/static/js/2.e576c186.chunk.js"
+  },
+  {
+    "revision": "1b4b62a1b06b78af50fa",
+    "url": "/resume/static/js/main.1c990b92.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "6fee44cf37849a8f12a842a908b47c14",
+    "url": "/resume/static/media/profile.6fee44cf.jpeg"
+  }
+]);
